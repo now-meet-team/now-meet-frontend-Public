d0fb6fa2ffca0d20d9eadf2a88588e9a4c1bc997
ky <ky@kyui-MacBookPro.local>
2023-02-07 00:03:05 +0900

    SETTING

--- ios/nowmeetfrontendTests/nowmeetfrontendTests.m
+++ ios/nowmeetfrontendTests/nowmeetfrontendTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface newMeetFrontendTests : XCTestCase
+@interface nowmeetfrontendTests : XCTestCase
 
 @end
 
-@implementation newMeetFrontendTests
+@implementation nowmeetfrontendTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
