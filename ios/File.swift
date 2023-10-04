4d06c2c4ac8838130ae6b2c95194f79825d05a04
kyday <kyday63@gmail.com>
2023-10-04 23:51:34 +0900

    Feat: 구글 맵 생성

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  nowmeetfrontend
+//
+//  Created by ky on 2023/10/04.
+//
+
+import Foundation
--- src/components/GoogleMap/GoogleMap.tsx
+++ src/components/GoogleMap/GoogleMap.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import {Platform, StyleSheet} from 'react-native';
-
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import React, {useEffect} from 'react';
+import {Platform, StyleSheet, View} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import {mapStyle} from './mapStyle';
 
 export default function GoogleMap() {
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      Geolocation.requestAuthorization('always');
+    }
+  }, []);
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+
+        console.log(latitude, longitude);
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  }, []);
+
   return (
-    <MapView
-      showsUserLocation
-      scrollEnabled={false}
-      toolbarEnabled={false}
-      mapType={Platform.OS === 'android' ? 'none' : 'standard'}
-      style={styles.map}
-      minZoomLevel={14}
-      initialRegion={{
-        latitude: 37.497386,
-        longitude: 127.026596,
-        latitudeDelta: 1.795218101812615,
-        longitudeDelta: 0.9008869173333,
-      }}
-    />
+    <>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        customMapStyle={mapStyle}
+        showsUserLocation
+        scrollEnabled={false}
+        toolbarEnabled={false}
+        mapType={Platform.OS === 'android' ? 'none' : 'standard'}
+        minZoomLevel={14}
+        initialRegion={{
+          latitude: 37.652753,
+          longitude: 126.901085,
+          latitudeDelta: 1.795218101812615,
+          longitudeDelta: 0.9008869173333,
+        }}
+      />
+    </>
   );
 }
 
